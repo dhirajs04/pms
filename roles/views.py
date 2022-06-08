e94a4230304fe5348011247a01aec252db6e518d
dhirajshah04 <dhiraj.shah04@gmail.com>
2022-06-08 16:24:13 +0545

    fix role list api error

--- roles/views.py
+++ roles/views.py
@@ -17,7 +17,7 @@ class RoleListView(generics.GenericAPIView):
     def get_serializer_class(self):
         return RoleSerializer
 
-    def get(self):
+    def get(self, request):
         queryset = self.filter_queryset(self.get_queryset())
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
