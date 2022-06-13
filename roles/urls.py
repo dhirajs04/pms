8acad6977d41d9dcdd796c9e454cf7d17a00ddde
dhirajshah04 <dhiraj.shah04@gmail.com>
2022-06-13 15:57:25 +0545

    add role create and edit api

--- roles/urls.py
+++ roles/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from roles.views import RoleListView, RoleDetailView
+from roles.views import RoleListView, RoleDetailView, RoleCreateView, RoleEditView
 
 app_name = 'roles'
 
 urlpatterns = [
-    path('list/', RoleListView.as_view(), name='RoleList'),
-    path('details/<int:pk>/', RoleDetailView.as_view(), name='RoleDetails')
+    path('list/', RoleListView.as_view(), name='list'),
+    path('details/<int:pk>/', RoleDetailView.as_view(), name='details'),
+    path('create/', RoleCreateView.as_view(), name='create'),
+    path('edit/<int:pk>', RoleEditView.as_view(), name='edit')
 ]
--- roles/views.py
+++ roles/views.py
@@ -1,12 +1,12 @@
-from rest_framework import generics, status, serializers
+from rest_framework import generics, status
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from roles.models import Role
-from roles.serializers import RoleSerializer
-from users.permissions import CanRead
+from roles.serializers import RoleSerializer, RoleCreateSerializer, RoleEditSerializer
+from users.permissions import CanRead, CanWrite, CanUpdate
 
 
 class RoleListView(generics.GenericAPIView):
@@ -39,3 +39,32 @@ class RoleDetailView(APIView):
         queryset = self.get_object(pk)
         serializer = RoleSerializer(queryset)
         return Response(serializer.data)
+
+
+class RoleCreateView(APIView):
+    permission_classes = [IsAuthenticated, CanWrite]
+
+    def post(self, request):
+        serializer = RoleCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RoleEditView(APIView):
+
+    permission_classes = [IsAuthenticated, CanUpdate]
+
+    def put(self, request, pk):
+        try:
+            role = Role.objects.get(pk=pk)
+        except Role.DoesNotExist:
+            raise NotFound(detail='Role Not Found')
+        serializer = RoleEditSerializer(instance=role, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
